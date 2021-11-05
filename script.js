d52915e0bc506bfdfbea138b024187a408004556
Chetonix <chetonix@gmail.com>
2021-11-05 15:32:44 +0530

    Add files to the game-repo

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+//Selecting Elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+
+//another way to select ids
+//const score1 = document.getElementById('score--1');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
