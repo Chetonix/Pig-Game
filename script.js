b8210f8561e4694a327f008b0812118905a628c7
Chetonix <chetonix@gmail.com>
2021-11-05 16:00:59 +0530

    add hidden class to the dice image for the start of the game

--- script.js
+++ script.js
@@ -3,9 +3,13 @@
 //Selecting Elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
+const diceEl = document.querySelector('.dice');
 
 //another way to select ids
 //const score1 = document.getElementById('score--1');
 
+//Making the scores be 0 at the start
 score0El.textContent = 0;
 score1El.textContent = 0;
+//Hidding the dice image at the start
+diceEl.classList.add('hidden');
