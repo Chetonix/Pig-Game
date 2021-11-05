cc10f7fe404d9ea78745f0f2acbb6f3c3cdbecd2
Chetonix <chetonix@gmail.com>
2021-11-05 16:14:17 +0530

    add a part of rollong dice functionality

--- script.js
+++ script.js
@@ -4,6 +4,9 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 //another way to select ids
 //const score1 = document.getElementById('score--1');
@@ -13,3 +16,6 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 //Hidding the dice image at the start
 diceEl.classList.add('hidden');
+
+//Rolling Dice Functionality
+btnRoll.addEventListener('click', function () {});
