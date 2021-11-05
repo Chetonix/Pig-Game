6828a269feeff7089f47b1552ac0e30f285207e4
Chetonix <chetonix@gmail.com>
2021-11-05 16:54:03 +0530

    Add in part the dice rolling functionality, specially part when dice 1 is not rolled

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 //Selecting Elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -11,7 +14,9 @@ const btnHold = document.querySelector('.btn--hold');
 //another way to select ids
 //const score1 = document.getElementById('score--1');
 
-//Making the scores be 0 at the start
+let currentScore = 0;
+
+//~START~ : Making the scores be 0 at the start
 score0El.textContent = 0;
 score1El.textContent = 0;
 //Hidding the dice image at the start
@@ -25,5 +30,13 @@ btnRoll.addEventListener('click', function () {
   //2. Display dice
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
-  //3. Check for rolled one; if true: switch the player
+
+  //3. Check for rolled one;
+  if (dice !== 1) {
+    //add dice to the current score
+    currentScore += dice;
+    current0El.textContent = currentScore; //Change later to be dynamic i.e. dispaly for the active player
+  } else {
+    // switch the player
+  }
 });
